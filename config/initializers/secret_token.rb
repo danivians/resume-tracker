3dcafa42ca62665c3b389cb5f39c88969504a212
Mike Fischer <mikefischer99@gmail.com>
2012-06-13 17:27:42 -0400

    Initial Rails structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResumeTracker::Application.config.secret_token = '85316fdf3e472621bd3c3c50b2bc241b2ed08b98831d688425a34668c8cbe54d59e3b9104f47816a3a0b8d79f718ee4248bf6a3ca5744b6d53690a15df34e282'
